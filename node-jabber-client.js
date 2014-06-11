b52a52677125b9025a2d0f4ab2dbc495afe28dd6
NeoAtlantis <rijndael@yeah.net>
2014-06-11 18:12:42 +0800

    save

--- node-jabber-client.js
+++ node-jabber-client.js
@@ -6,3 +6,66 @@
  *      supervisor node-jabber-client.js
  */
 require('./lib');
+
+var port = process.argv[2] || 10086;
+
+function mimeType(filename){
+    var map = {
+        '.js': 'application/x-javascript',
+        '.css': 'text/css',
+        '.html': 'text/html',
+        '.jpg': 'image/jpeg',
+        '.gif': 'image/gif',
+        '.ttf': 'application/x-font-ttf',
+    };
+    for(var endung in map)
+        if(filename.endsWith(endung)) return map[endung];
+    return 'text/plain';
+};
+ 
+$.node.http.createServer(function(request, response) {
+    var pathname = $.node.url.parse(request.url).pathname,
+        uri = '.' + pathname,
+        filename = $.tools.resolve('web', uri),
+        header = function(){return {
+            'Date': new Date().toGMTString(),
+            'Content-Type': 'text/plain',
+        };};
+
+    if('/~' == pathname) return new _process(request, response);
+
+    // static file server
+    
+    console.log('Accessing:', filename);
+    
+    $.node.fs.exists(filename, function(exists) {
+        if(!exists) {
+            response.writeHead(404, header());
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        };
+     
+        if($.node.fs.statSync(filename).isDirectory())
+            filename = $.tools.resolve('web', uri, 'index.html');
+     
+        $.node.fs.readFile(filename, "binary", function(err, file){
+            var gotHeader = header();
+            if(err) {
+                response.writeHead(500, gotHeader);
+                response.write(err + "\n");
+                response.end();
+                return;
+            };
+            gotHeader['Content-Type'] = mimeType(filename);
+            response.writeHead(200, gotHeader);
+            response.write(file, "binary");
+            response.end();
+        });
+    });
+}).listen(parseInt(port, 10));
+
+//////////////////////////////////////////////////////////////////////////////
+
+function _process(req, res){
+};
