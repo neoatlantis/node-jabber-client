15c8b996dba2a7af0bbf961496bd095548c74e32
NeoAtlantis <rijndael@yeah.net>
2014-06-14 23:17:52 +0800

    save

--- web/js/windowsManager.js
+++ web/js/windowsManager.js
@@ -1,11 +1,20 @@
 WM = {}; // Windows Manager
 
-WM.register = function(title, content){
+WM.register = function(title, div){
     var handle = COMMON.uuid();
-    $(content).dialog().attr('data-uuid', handle);
+
+    div
+        .attr('title', title)
+        .attr('data-uuid', handle)
+        .appendTo('body')
+        .dialog()
+        .on("dialogclose", function(event, ui){
+            $('[data-uuid="' + handle + '"]').remove();
+        });
+    ;
 
     $('<li>')
-        .data('uuid', handle)
+        .attr('data-uuid', handle)
         .append($('<a>', {href: '#'})
             .text(title)
             .prepend($('<span>')
@@ -13,24 +22,27 @@ WM.register = function(title, content){
             )
         )
         .click(function(){
-            var uuid = $(this).data('uuid');
+            var uuid = $(this).attr('data-uuid');
             $('#nav-switches').children('li').each(function(){
-                var thisUUID = $(this).data('uuid');
+                var thisUUID = $(this).attr('data-uuid');
                 if(uuid != thisUUID) return $(this).removeClass('active');
-
+                
+                var thisDialog = $('div[data-uuid="' + uuid + '"]');
                 if($(this).hasClass('active')){
                     // hide this dialog
+                    $(this).removeClass('active');
+                    thisDialog.hide();
                 } else {
                     // show this dialog
                     $(this).addClass('active');
-                    $('[data-uuid="' + uuid + '"])
+                    thisDialog
                         .show()
                         .dialog('moveToTop')
                     ;
                 };
             });
-
         })
+        .click()
         .appendTo($('#nav-switches'))
     ;
 };
